c87da8520077c93e58a702c951c114d0f067eec4
Кристина Гуляева <kristinagul.24@gmail.com>
2022-10-19 09:49:25 +1000

    ieuthkdhfkj

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -34,22 +34,27 @@ namespace WpfApp1ИСРПО_Гуляева_пр_8
             double dx = Convert.ToDouble(Dx.Text);
             double a = Convert.ToDouble(A.Text);
 
+
             // Цикл для табулирования функции
 
             double x = x0;
 
-            while (x<= (xk + dx / 2)) 
+            while (x <= (xk + dx / 2))
             {
                 double y = a * Math.Log(x);
 
-               X.Text += "x=" + Convert.ToString(x) + Environment.NewLine;
-               Y.Text += "y=" + Convert.ToString(x) + Environment.NewLine;
-                
+                X.Text += "x=" + Convert.ToString(x) + Environment.NewLine;
+                Y.Text += "y=" + Convert.ToString(x) + Environment.NewLine;
+
+
                 x = x + dx;
+
+
 
             }
         }
-
     }
+
 }
+        
 
